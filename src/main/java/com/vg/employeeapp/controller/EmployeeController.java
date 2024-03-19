b0477fa592aba07c372ad69ef840fc97c199be47
Nilofer Mehta <nilofer.mehta@gmail.com>
2024-03-19 14:07:08 -0400

    Setup Employee API

--- src/main/java/com/vg/employeeapp/controller/EmployeeController.java
+++ src/main/java/com/vg/employeeapp/controller/EmployeeController.java
@@ -1,17 +1,13 @@
 package com.vg.employeeapp.controller;
 
-import com.vg.employeeapp.entity.Employee;
-import com.vg.employeeapp.response.AddressResponse;
 import com.vg.employeeapp.response.EmployeeResponse;
+import com.vg.employeeapp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import com.vg.employeeapp.service.EmployeeService;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class EmployeeController {
